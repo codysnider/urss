610914d4d65402b774a59d34f8febc74463b53c6
Andrew Dolgov <noreply@fakecake.org>
2015-08-14 08:51:53 +0300

    af_comics: add pvponline

---
+++ plugins/af_comics/filters/af_comics_pvp.php
@@ -0,0 +1,33 @@
+<?php
+class Af_Comics_Pvp extends Af_ComicFilter {
+
+	function supported() {
+		return array("PvP Online");
+	}
+
+	function process(&$article) {
+		if (strpos($article["guid"], "pvponline.com") !== FALSE) {
+
+				$res = fetch_file_contents($article["link"], false, false, false,
+					 false, false, 0,
+					 "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)");
+
+				$doc = new DOMDocument();
+				@$doc->loadHTML($res);
+
+				if ($doc) {
+					$xpath = new DOMXPath($doc);
+					$basenode = $xpath->query('//section[@class="comic-art"]')->item(0);
+
+					if ($basenode) {
+						$article["content"] = $doc->saveXML($basenode);
+					}
+				}
+
+			 return true;
+		}
+
+		return false;
+	}
+}
+?>
