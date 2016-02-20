94d425fe4a21d41737fe656d9c54c38775fbd132
Andrew Dolgov <noreply@madoka.volgo-balt.ru>
2016-02-20 12:06:47 +0300

    rewrite_relative_url: only skip urls like magnet: instead of everything with :

--- include/functions2.php
+++ include/functions2.php
@@ -2048,15 +2048,15 @@
 	 * @return string Absolute URL
 	 */
 	function rewrite_relative_url($url, $rel_url) {
-		if (strpos($rel_url, ":") !== false) {
-			return $rel_url;
-		} else if (strpos($rel_url, "://") !== false) {
+		if (strpos($rel_url, "://") !== false) {
 			return $rel_url;
 		} else if (strpos($rel_url, "//") === 0) {
 			# protocol-relative URL (rare but they exist)
 			return $rel_url;
-		} else if (strpos($rel_url, "/") === 0)
-		{
+		} else if (preg_match("/^[a-z]+:/i", $rel_url)) {
+			# magnet:, feed:, etc
+			return $rel_url;
+		} else if (strpos($rel_url, "/") === 0) {
 			$parts = parse_url($url);
 			$parts['path'] = $rel_url;
 
