c2744831cd319fb967a83dc643bdb8b25f6c0b02
Andrew Dolgov <noreply@fakecake.org>
2017-04-26 21:15:43 +0300

    phpmd ci: add plugins

--- utils/gitlab-ci/phpmd.sh
+++ utils/gitlab-ci/phpmd.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-phpmd include,classes text utils/gitlab-ci/phpmd-ruleset.xml
+phpmd include,classes,plugins text utils/gitlab-ci/phpmd-ruleset.xml
 
 FILES=$(ls -dm *.php | tr -d " "| tr -d "\n")
 phpmd $FILES text utils/gitlab-ci/phpmd-ruleset.xml
