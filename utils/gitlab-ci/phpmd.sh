39238155802534f9234835e9fce0fd9c5348221e
Andrew Dolgov <noreply@fakecake.org>
2017-04-26 16:26:53 +0300

    ci: check syntax via php first

--- utils/gitlab-ci/phpmd.sh
+++ utils/gitlab-ci/phpmd.sh
@@ -1,7 +1,8 @@
-#!/bin/sh -e
+#!/bin/sh -i -e
 
 #phpmd include text utils/gitlab-ci/phpmd-ruleset.xml
 #phpmd classes text utils/gitlab-ci/phpmd-ruleset.xml
 
 FILES=$(ls -dm *.php | tr -d " "| tr -d "\n")
 phpmd $FILES text utils/gitlab-ci/phpmd-ruleset.xml
+
