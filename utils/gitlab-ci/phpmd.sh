cffe1d51514bde0eda8e37e08183d30b29c4c79b
Andrew Dolgov <noreply@fakecake.org>
2017-04-26 16:29:50 +0300

    enable phpmd for all components

--- utils/gitlab-ci/phpmd.sh
+++ utils/gitlab-ci/phpmd.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -i -e
 
-#phpmd include text utils/gitlab-ci/phpmd-ruleset.xml
-#phpmd classes text utils/gitlab-ci/phpmd-ruleset.xml
+phpmd include,classes text utils/gitlab-ci/phpmd-ruleset.xml
 
 FILES=$(ls -dm *.php | tr -d " "| tr -d "\n")
 phpmd $FILES text utils/gitlab-ci/phpmd-ruleset.xml
