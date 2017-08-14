edefcec19d43c261c5bd5ee7382149581757058e
Andrew Dolgov <noreply@fakecake.org>
2017-08-14 22:29:50 +0300

    filters/edit: properly parse feed-specific legacy format filter rules

--- classes/pref/filters.php
+++ classes/pref/filters.php
@@ -409,7 +409,7 @@ class Pref_Filters extends Handler_Protected {
                 if (sql_bool_to_bool($line["cat_filter"])) {
                     $feed_id = "CAT:" . (int)$line["cat_id"];
                 } else {
-                    $feed_id = (int)$line["cat_id"];
+                    $feed_id = (int)$line["feed_id"];
                 }
 
                 $line["feed_id"] = ["" . $feed_id]; // set item type to string for in_array()
