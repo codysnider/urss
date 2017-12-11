31f7966567d800f02bab0fa43c0a01ff2dfd5653
Andrew Dolgov <noreply@fakecake.org>
2017-12-11 19:04:06 +0300

    use button-style links for bookmarklets

--- plugins/bookmarklets/init.php
+++ plugins/bookmarklets/init.php
@@ -28,7 +28,7 @@ class Bookmarklets extends Plugin {
 		$bm_url = htmlspecialchars("javascript:{if(confirm('$confirm_str'.replace('%s',window.location.href)))window.location.href='$bm_subscribe_url'+window.location.href}");
 
 		print "<p>";
-		print "<a href=\"$bm_url\" class='bookmarklet'>" . __('Subscribe in Tiny Tiny RSS'). "</a>";
+		print "<a href=\"$bm_url\" class='dijitButton'>" . __('Subscribe in Tiny Tiny RSS'). "</a>";
 		print "</p>";
 
 		print "<p>" . __("Use this bookmarklet to publish arbitrary pages using Tiny Tiny RSS") . "</p>";
@@ -37,7 +37,7 @@ class Bookmarklets extends Plugin {
 
 		$bm_url = htmlspecialchars("javascript:(function(){var d=document,w=window,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),f='".get_self_url_prefix()."/public.php?op=sharepopup',l=d.location,e=encodeURIComponent,g=f+'&title='+((e(s))?e(s):e(document.title))+'&url='+e(l.href);function a(){if(!w.open(g,'t','toolbar=0,resizable=0,scrollbars=1,status=1,width=500,height=250')){l.href=g;}}a();})()");
 
-		print "<a href=\"$bm_url\" class='bookmarklet'>" . __('Share with Tiny Tiny RSS'). "</a>";
+		print "<a href=\"$bm_url\" class='dijitButton'>" . __('Share with Tiny Tiny RSS'). "</a>";
 
 		print "</p>";
 
