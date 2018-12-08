b96beeeda772fe087471fc9de4857702b6abe096
Andrew Dolgov <noreply@fakecake.org>
2018-12-08 15:55:35 +0300

    bookmarklets: show pref link-buttons properly

--- plugins/bookmarklets/init.php
+++ plugins/bookmarklets/init.php
@@ -28,19 +28,16 @@ class Bookmarklets extends Plugin {
 
 		$bm_url = htmlspecialchars("javascript:{if(confirm('$confirm_str'.replace('%s',window.location.href)))window.location.href='$bm_subscribe_url'+window.location.href}");
 
-		print "<p>";
-		print "<a href=\"$bm_url\" class='dijitButton'>" . __('Subscribe in Tiny Tiny RSS'). "</a>";
-		print "</p>";
+		print "<p><label class='dijitButton'>";
+		print "<a href=\"$bm_url\">" . __('Subscribe in Tiny Tiny RSS'). "</a>";
+		print "</label></p>";
 
 		print "<p>" . __("Use this bookmarklet to publish arbitrary pages using Tiny Tiny RSS") . "</p>";
 
-		print "<p>";
-
+		print "<p><label class='dijitButton'>";
 		$bm_url = htmlspecialchars("javascript:(function(){var d=document,w=window,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),f='".get_self_url_prefix()."/public.php?op=sharepopup',l=d.location,e=encodeURIComponent,g=f+'&title='+((e(s))?e(s):e(document.title))+'&url='+e(l.href);function a(){if(!w.open(g,'t','toolbar=0,resizable=0,scrollbars=1,status=1,width=500,height=250')){l.href=g;}}a();})()");
-
-		print "<a href=\"$bm_url\" class='dijitButton'>" . __('Share with Tiny Tiny RSS'). "</a>";
-
-		print "</p>";
+		print "<a href=\"$bm_url\">" . __('Share with Tiny Tiny RSS'). "</a>";
+		print "</label></p>";
 
 		print "</div>"; #pane
 
