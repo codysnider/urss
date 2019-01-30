57c3322088c1cf47cef82b90c83bec558224e5db
Andrii Zhuk <Andrii_Zhuk@epam.com>
2019-01-30 16:14:07 +0200

    Changed language name for Ukrainian

--- include/functions.php
+++ include/functions.php
@@ -105,7 +105,7 @@
 					"pt_PT" => "Portuguese/Portugal",
 					"zh_CN" => "Simplified Chinese",
 					"zh_TW" => "Traditional Chinese",
-					"uk_UA" => "Украинский",
+					"uk_UA" => "Українська",
 					"sv_SE" => "Svenska",
 					"fi_FI" => "Suomi",
 					"tr_TR" => "Türkçe");
