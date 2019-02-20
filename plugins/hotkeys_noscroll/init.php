1a8770f8f48aeb7fbbcd3b569bd37a4a88ccfdb4
Andrew Dolgov <noreply@fakecake.org>
2019-02-20 15:32:53 +0300

    add plugins/hotkeys_noscroll

---
+++ plugins/hotkeys_noscroll/init.php
@@ -0,0 +1,31 @@
+<?php
+class Hotkeys_Noscroll extends Plugin {
+	private $host;
+
+	function about() {
+		return array(1.0,
+			"n/p hotkeys move between articles without scrolling",
+			"fox");
+	}
+
+	function init($host) {
+		$this->host = $host;
+
+		$host->add_hook($host::HOOK_HOTKEY_MAP, $this);
+	}
+
+	function hook_hotkey_map($hotkeys) {
+
+		$hotkeys["(40)|down"] = "next_article_noscroll";
+		$hotkeys["(38)|up"] = "prev_article_noscroll";
+		$hotkeys["n"] = "next_article_noscroll";
+		$hotkeys["p"] = "prev_article_noscroll";
+
+		return $hotkeys;
+	}
+
+	function api_version() {
+		return 2;
+	}
+
+}
