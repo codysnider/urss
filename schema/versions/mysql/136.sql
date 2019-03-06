c8fc9eee0c5d7a202bd79db41292859c69a79347
Andrew Dolgov <noreply@fakecake.org>
2019-03-06 21:35:34 +0300

    update mysql 136 script

--- schema/versions/mysql/136.sql
+++ schema/versions/mysql/136.sql
@@ -2,7 +2,7 @@ begin;
 
 alter table ttrss_archived_feeds add column created datetime;
 update ttrss_archived_feeds set created = NOW();
-alter table ttrss_archived_feeds change created created bool not null;
+alter table ttrss_archived_feeds change created created datetime not null;
 
 update ttrss_version set schema_version = 136;
 
