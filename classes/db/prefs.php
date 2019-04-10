e31e9ae04d7d3dd9e7a4633badba60d8229eee85
Andrew Dolgov <noreply@fakecake.org>
2019-04-10 13:39:07 +0300

    db_prefs: don't report missing prefs if die_on_error is false

--- classes/db/prefs.php
+++ classes/db/prefs.php
@@ -90,8 +90,8 @@ class Db_Prefs {
 
 			return $this->convert($value, $type_name);
 
-		} else {
-			user_error("Fatal error, unknown preferences key: $pref_name (owner: $user_id)", $die_on_error ? E_USER_ERROR : E_USER_WARNING);
+		} else if ($die_on_error) {
+			user_error("Fatal error, unknown preferences key: $pref_name (owner: $user_id)", E_USER_ERROR);
 			return null;
 		}
 	}
