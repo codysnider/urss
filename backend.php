0697eca0e111a194f15f12025ee1859b0f32843b
Andrew Dolgov <noreply@fakecake.org>
2019-12-06 07:34:50 +0300

    remove testing for get_magic_quotes_gpc: deprecated in php7.4, apparently not working since php 5.4

--- backend.php
+++ backend.php
@@ -2,21 +2,6 @@
 	set_include_path(dirname(__FILE__) ."/include" . PATH_SEPARATOR .
 		get_include_path());
 
-	/* remove ill effects of magic quotes */
-
-	if (get_magic_quotes_gpc()) {
-		function stripslashes_deep($value) {
-			$value = is_array($value) ?
-				array_map('stripslashes_deep', $value) : stripslashes($value);
-				return $value;
-		}
-
-		$_POST = array_map('stripslashes_deep', $_POST);
-		$_GET = array_map('stripslashes_deep', $_GET);
-		$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
-		$_REQUEST = array_map('stripslashes_deep', $_REQUEST);
-	}
-
 	$op = $_REQUEST["op"];
 	@$method = $_REQUEST['subop'] ? $_REQUEST['subop'] : $_REQUEST["method"];
 
--- public.php
+++ public.php
@@ -2,21 +2,6 @@
 	set_include_path(dirname(__FILE__) ."/include" . PATH_SEPARATOR .
 		get_include_path());
 
-	/* remove ill effects of magic quotes */
-
-	if (get_magic_quotes_gpc()) {
-		function stripslashes_deep($value) {
-			$value = is_array($value) ?
-				array_map('stripslashes_deep', $value) : stripslashes($value);
-				return $value;
-		}
-
-		$_POST = array_map('stripslashes_deep', $_POST);
-		$_GET = array_map('stripslashes_deep', $_GET);
-		$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
-		$_REQUEST = array_map('stripslashes_deep', $_REQUEST);
-	}
-
 	require_once "autoload.php";
 	require_once "sessions.php";
 	require_once "functions.php";
